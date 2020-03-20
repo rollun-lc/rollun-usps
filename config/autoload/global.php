1dd913f52c086863ffe9927447c465bcb7d1cec8
r.ratsun <r.ratsun@gmail.com>
2020-03-20 13:46:06 +0200

    Cleanup config

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -5,37 +5,4 @@
  * @license LICENSE.md New BSD License
  */
 
-use rollun\callback\Middleware\CallablePluginManagerFactory;
-use rollun\Entity\Shipping\Method\Usps\UspsProvider;
-use service\Entity\Rollun\Shipping\Method\Provider\RmPrepCenter;
-use service\Entity\Rollun\Shipping\Method\Provider\Root as RootProvider;
-use service\Entity\Api\DataStore\Shipping\AllCosts;
-
-
-return [
-    'dependencies' => [
-        'invokables' => [
-            'Usps' => UspsProvider::class,
-            'shipping-all-costs' => AllCosts::class
-        ],
-    ],
-    'aliases' => [
-        RootProvider::class => 'Root'
-    ],
-    'ShippingMethod' => [
-        'RmPrepCntr' => [
-            'class' => RmPrepCenter::class,
-            'shortName' => 'RmPrepCntr',
-            'shippingMethodList' => [
-                'Usps'
-            ]
-        ],
-        'Root' => [
-            'class' => RootProvider::class,
-            'shortName' => 'Root',
-            'shippingMethodList' => [
-                'RmPrepCntr'
-            ]
-        ],
-    ],
-];
+return [];
