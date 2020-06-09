22b912909f75df7f41a7bae850261ae11effea1a
r.ratsun <r.ratsun.rollun@gmail.com>
2020-06-09 09:45:53 +0300

    .

--- bin/csv_generator.php
+++ bin/csv_generator.php
@@ -25,49 +25,39 @@ $container->setService(LifeCycleToken::class, $lifeCycleToken);
 /** @var AllCosts $allCosts */
 $allCosts = $container->get(AllCosts::class);
 
-$content = [
-    [
-        'rollun_id', 'weight', 'height', 'width', 'depth', 'from', 'to', 'shipping_method_1', 'shipping_cost_1', 'shipping_method_2', 'shipping_cost_2', 'shipping_method_3', 'shipping_cost_3', 'shipping_method_4', 'shipping_cost_4'
-    ]
-];
+$content = [];
 
-$file = fopen('data/USPSprice.csv', 'r');
-while (($line = fgetcsv($file, 99999, ";")) !== false) {
-    if ($line[0] !== 'rollun_id') {
-        $res = $allCosts->query($allCosts->buildUspShippingQuery($line[5], $line[6], (float)$line[1], (float)$line[3], (float)$line[4], (float)$line[2]));
-        $row = $line;
+$file = fopen('data/Priority_products.csv', 'r');
+while (($line = fgetcsv($file, 99999, ",")) !== false) {
+    $res = $allCosts->query($allCosts->buildUspShippingQuery('84663', '78228', (float)$line[1], (float)$line[3], (float)$line[4], (float)$line[2]));
+    $row = $line;
 
-        if (isset($res[0])) {
-            $row[] = $res[0]['id'];
-            $row[] = $res[0]['cost'];
-        }
-        if (isset($res[1])) {
-            $row[] = $res[1]['id'];
-            $row[] = $res[1]['cost'];
-        }
-        if (isset($res[2])) {
-            $row[] = $res[2]['id'];
-            $row[] = $res[2]['cost'];
-        }
-        if (isset($res[3])) {
-            $row[] = $res[3]['id'];
-            $row[] = $res[3]['cost'];
-        }
-        $content[] = $row;
+    if (isset($res[0])) {
+        $row[] = str_replace('Root-RM-PickUp-Usps-', '', $res[0]['id']);
+        $row[] = $res[0]['cost'];
     }
+    if (isset($res[1])) {
+        $row[] = str_replace('Root-RM-PickUp-Usps-', '', $res[1]['id']);
+        $row[] = $res[1]['cost'];
+    }
+    if (isset($res[2])) {
+        $row[] = str_replace('Root-RM-PickUp-Usps-', '', $res[2]['id']);
+        $row[] = $res[2]['cost'];
+    }
+    $content[] = $row;
 }
 fclose($file);
 
 // Open a file in write mode ('w')
-$fp = fopen('data/USPSprice_processed.csv', 'w');
+$fp = fopen('data/Priority_products_processed.csv', 'w');
 
 // Loop through file pointer and a line
 foreach ($content as $fields) {
-    fputcsv($fp, $fields, ';');
+    fputcsv($fp, $fields, ',');
 }
 
 fclose($fp);
 
 echo '<pre>';
-print_r('123');
+print_r('Done! ' . count($content));
 die();
