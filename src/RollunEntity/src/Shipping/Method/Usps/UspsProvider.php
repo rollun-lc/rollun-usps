0525479122c4eec5da93a0f2ec72a97d933cc845
r.ratsun <r.ratsun.rollun@gmail.com>
2020-06-15 12:59:08 +0300

    Priority Mail Cubic

--- src/RollunEntity/src/Shipping/Method/Usps/UspsProvider.php
+++ src/RollunEntity/src/Shipping/Method/Usps/UspsProvider.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace rollun\Entity\Shipping\Method\Usps;
 
+use rollun\Entity\Shipping\Method\Usps\PriorityMail\Cubic;
 use rollun\Entity\Shipping\Method\Usps\PriorityMail\FlatRate;
 use rollun\Entity\Shipping\Method\Usps\FirstClass\Package;
 use rollun\Entity\Shipping\Method\Usps\PriorityMail\RegionalRate;
@@ -32,7 +33,7 @@ class UspsProvider extends ShippingMethodProvider
     {
         if ($shippingMethods === null) {
             $shippingMethods = [];
-            $classes = [FlatRate::class, Package::class, RegionalRate::class, Regular::class];
+            $classes = [FlatRate::class, Package::class, RegionalRate::class, Regular::class, Cubic::class];
             foreach ($classes as $oneClass) {
                 $shortNames = $oneClass::getAllShortNames();
                 foreach ($shortNames as $shortName) {
