315d1534ea93a1a6fa695f5545116cd8e0473272
Maria <m.zelin.rollun@gmail.com>
2022-05-01 14:51:38 +0300

    feat(10263): add flatRateGroup and largePackageSurcharge for AU

--- src/RollunEntity/src/Shipping/Method/DropShip/AuDropShip.php
+++ src/RollunEntity/src/Shipping/Method/DropShip/AuDropShip.php
@@ -43,9 +43,7 @@ class AuDropShip extends LevelBasedShippingMethod
      */
     public function getCost(ShippingRequest $shippingRequest, $shippingDataOnly = false)
     {
-        $isFirstClass = $shippingRequest->getAttribute('isFirstClass');
-
-        if ($isFirstClass || (is_null($isFirstClass) && $shippingRequest->item->getWeight() < 0.8)) {
+        if ($this->isFirstClass($shippingRequest)) {
             // Если вес меньше 0.8 и вмещается в First-class - цена будет First-class
             $request = clone $shippingRequest;
             $request->addressOrigination = new Address(null, self::ZIP_FROM);
@@ -54,11 +52,21 @@ class AuDropShip extends LevelBasedShippingMethod
                 const USPS_BOXES = [['FtCls-Package', 'First-Class Package Service', 'FIRST CLASS COMMERCIAL', 'PACKAGE SERVICE', '', 22, 18, 15, 0.899]];
             };
             if ($firstClass->canBeShipped($request)) {
-                return $firstClass->getCost($request, $shippingDataOnly);
+                $shippingCost = $firstClass->getCost($request, $shippingDataOnly);
             }
         }
 
-        return parent::getCost($shippingRequest, $shippingDataOnly);
+        if (!isset($shippingCost)) {
+            $shippingCost = $this->getStaticShippingCost($shippingRequest);
+        }
+
+        $largePackageSurcharge = $shippingRequest->getAttribute('largePackageSurcharge');
+
+        if (!is_null($largePackageSurcharge)) {
+            $shippingCost += $largePackageSurcharge;
+        }
+
+        return $shippingCost;
     }
 
     /**
@@ -75,5 +83,41 @@ class AuDropShip extends LevelBasedShippingMethod
     protected function getLevelCost(array $level): ?float
     {
         return $level[1];
+    }
+
+    private function getStaticShippingCost(ShippingRequest $shippingRequest): float
+    {
+        $flatRateGroup = $shippingRequest->getAttribute('flatRateGroup');
+
+        switch ($flatRateGroup) {
+            case 'ATV25':
+                return 14.95;
+            case 'ATV30':
+                return 17.95;
+            case 'ATV31':
+                return 21.95;
+            case 'LARGE':
+                return 15;
+            case 'MC1':
+                return 9.95;
+            case 'RIMS':
+                return 10.5;
+            case 'WES90':
+            case 'WES91 ':
+                return 29.95;
+            default:
+                return 9.5;
+        }
+    }
+
+    private function isFirstClass(ShippingRequest $shippingRequest): bool
+    {
+        $firstClass = $shippingRequest->getAttribute('isFirstClass');
+
+        if (is_null($firstClass)) {
+            return $shippingRequest->item->getWeight() < 0.8;
+        }
+
+        return $firstClass;
     }
 }
