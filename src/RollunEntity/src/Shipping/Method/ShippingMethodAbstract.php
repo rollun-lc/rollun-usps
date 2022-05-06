62efedda5165621847a5fa004e6ef454a3354fbe
Zuiev Mykhailo <zuiev.rollun@gmail.com>
2022-05-06 15:02:37 +0300

    refactor: delegate getting the zone to DomesticZoneService
    Move responsibility for getting the zone from ShippingMethodAbstract to DomesticZoneService

--- src/RollunEntity/src/Shipping/Method/ShippingMethodAbstract.php
+++ src/RollunEntity/src/Shipping/Method/ShippingMethodAbstract.php
@@ -49,17 +49,28 @@ abstract class ShippingMethodAbstract implements ShippingMethodInterface, Shippi
     protected $canShipDangerous = true;
 
     /**
+     * @var DomesticZoneService
+     */
+    private $domesticZoneService;
+
+    /**
      * ShippingMethodAbstract constructor.
      *
      * @param ProductContainerInterface $container
-     * @param string                    $shortName
+     * @param string $shortName
      * @param                           $maxWeight
+     * @param DomesticZoneService|null $domesticZoneService
      */
-    public function __construct(ProductContainerInterface $container, string $shortName, $maxWeight)
-    {
+    public function __construct(
+        ProductContainerInterface $container,
+        string $shortName,
+        $maxWeight,
+        DomesticZoneService $domesticZoneService = null
+    ){
         $this->shortName = $shortName;
         $this->maxWeight = $maxWeight;
         $this->container = $container;
+        $this->domesticZoneService = $domesticZoneService ?? $this->createDomesticZoneService();
     }
 
     /**
@@ -170,103 +181,12 @@ abstract class ShippingMethodAbstract implements ShippingMethodInterface, Shippi
      */
     public function getZone(string $zipFrom, string $zipTo): int
     {
-        // get zones
-        $data = $this->getDomesticZones($zipFrom);
-
-        // prepare destinationZip
-        $destinationZip = (int)$this->getTreeDigitsZip($zipTo);
-
-        if (!isset($data['zipCodes'][$destinationZip])) {
-            throw new \Exception('No such zip zone');
-        }
-
-        return $data['zipCodes'][$destinationZip];
+        return $this->domesticZoneService->getZone($zipFrom, $zipTo);
     }
 
-    /**
-     * @param string $zipFrom
-     *
-     * @return array
-     * @throws \Exception
-     */
-    protected function getDomesticZones(string $zipFrom): array
+    private function createDomesticZoneService(): DomesticZoneService
     {
-        // prepare 3-digits ZIP Code
-        $treeDigitsZip = $this->getTreeDigitsZip($zipFrom);
-
-        // prepare file name
-        $fileName = sprintf(self::DOMESTIC_ZONE_FILE, $treeDigitsZip);
-
-        // create file if not exists
-        if (!file_exists($fileName)) {
-            $data = $this->createDomesticZoneFile($treeDigitsZip, $fileName);
-        } else {
-            $data = json_decode(file_get_contents($fileName), true);
-        }
-
-        return $data;
-    }
-
-    /**
-     * @param string $treeDigitsZip
-     * @param string $fileName
-     *
-     * @return array
-     * @throws \Exception
-     */
-    protected function createDomesticZoneFile(string $treeDigitsZip, string $fileName): array
-    {
-        $content = @file_get_contents(sprintf(self::DOMESTIC_ZONE_API, $treeDigitsZip, str_replace('_', '%2F', (new \DateTime())->format('m_d_Y'))));
-        if (!empty($content)) {
-            // parse content
-            $content = json_decode($content, true);
-
-            $i = 0;
-            while (isset($content["Column$i"])) {
-                foreach ($content["Column$i"] as $row) {
-                    $parts = explode('---', $row['ZipCodes']);
-                    if (isset($parts[1])) {
-                        $from = (int)$parts[0];
-                        $to = (int)$parts[1];
-                        while ($from <= $to) {
-                            $data['zipCodes'][$from] = (int)$row['Zone'];
-                            $from++;
-                        }
-                    } else {
-                        $data['zipCodes'][(int)$row['ZipCodes']] = (int)$row['Zone'];
-                    }
-                }
-                $i++;
-            }
-
-            // create dir if not exist
-            if (!file_exists('data')) {
-                mkdir('data', 0777, true);
-            }
-
-            if (empty($data)) {
-                throw new \Exception('API response parsing failed');
-            } else {
-                $data['createdAt'] = (new \DateTime())->format('Y-m-d H:i:s');
-            }
-
-            // create file
-            file_put_contents($fileName, json_encode($data));
-        } else {
-            throw new \Exception('Domestic zone API unavailable');
-        }
-
-        return $data;
-    }
-
-    /**
-     * @param string $zipCode
-     *
-     * @return string
-     */
-    protected function getTreeDigitsZip(string $zipCode): string
-    {
-        return mb_substr($zipCode, 0, 3);
+        return new DomesticZoneService(self::DOMESTIC_ZONE_API, self::DOMESTIC_ZONE_FILE);
     }
 
     /**
