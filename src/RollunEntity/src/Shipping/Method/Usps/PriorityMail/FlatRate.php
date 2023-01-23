d2851beaea89e825431c89c05942e89c4ad84f59
Maria <m.zelin.rollun@gmail.com>
2023-01-23 19:36:13 +0200

    fix(16332): change USPS prices

--- src/RollunEntity/src/Shipping/Method/Usps/PriorityMail/FlatRate.php
+++ src/RollunEntity/src/Shipping/Method/Usps/PriorityMail/FlatRate.php
@@ -27,9 +27,10 @@ class FlatRate extends ShippingsAbstract
      * (or https://pe.usps.com/text/dmm300/Notice123.htm#_c078)
      */
     const USPS_BOXES = [
-        ['PM-FR-Env', 'Priority Mail Flat Rate Envelope', 'PRIORITY COMMERCIAL', '', 'FLAT RATE ENVELOPE', 12.5, 9.5, 0, 70, 8.95],
-        ['PM-FR-LegalEnv', 'Priority Mail Legal Flat Rate Envelope', 'PRIORITY COMMERCIAL', '', 'LEGAL FLAT RATE ENVELOPE', 15, 9.5, 0, 70, 9.25],
-        ['PM-FR-Pad-Env', 'Priority Mail Flat Rate Padded Envelope', 'PRIORITY COMMERCIAL', '', 'PADDED FLAT RATE ENVELOPE', 12.5, 9.5, 0, 70, 9.65],
+        // price - last column
+        ['PM-FR-Env', 'Priority Mail Flat Rate Envelope', 'PRIORITY COMMERCIAL', '', 'FLAT RATE ENVELOPE', 12.5, 9.5, 0, 70, 8.05],
+        ['PM-FR-LegalEnv', 'Priority Mail Legal Flat Rate Envelope', 'PRIORITY COMMERCIAL', '', 'LEGAL FLAT RATE ENVELOPE', 15, 9.5, 0, 70, 8.35],
+        ['PM-FR-Pad-Env', 'Priority Mail Flat Rate Padded Envelope', 'PRIORITY COMMERCIAL', '', 'PADDED FLAT RATE ENVELOPE', 12.5, 9.5, 0, 70, 8.80],
 //        ['PM-FR-SmBox', 'Priority Mail Small Flat Rate Box', 'PRIORITY COMMERCIAL', '', 'SM FLAT RATE BOX', 8.625, 5.375, 1.625, 70, 9.45],
 //        ['PM-FR-MdBox1', 'Priority Mail Medium Flat Rate Box', 'PRIORITY COMMERCIAL', '', 'MD FLAT RATE BOX', 11, 8.5, 5.5, 70, 16.10],
 //        ['PM-FR-MdBox2', 'Priority Mail Medium Flat Rate Box', 'PRIORITY COMMERCIAL', '', 'MD FLAT RATE BOX', 13.625, 11.875, 3.375, 70, 16.10],
